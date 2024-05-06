e8fa5dd37f17ad03cc086cebc4746dc297d859a9
pitchblendelegit <72884743+pitchblendelegit@users.noreply.github.com>
2024-05-06 16:42:30 +0700

    update order

--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -2,73 +2,46 @@ import React from 'react';
 import { Space, Table, Tag } from 'antd';
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: 'Products',
+    dataIndex: 'product',
+    key: 'product',
     render: (text) => <a>{text}</a>,
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'Order Value',
+    dataIndex: 'orderValue',
+    key: 'orderValue',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Quantity',
+    dataIndex: 'quantity',
+    key: 'quantity',
   },
   {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
+    title: 'Order ID',
+    dataIndex: 'orderId',
+    key: 'orderId',
   },
   {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
+    title: 'Expected Delivery',
+    dataIndex: 'expectedDelivery',
+    key: 'expectedDelivery',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
   },
 ];
 const data = [
   {
     key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    product: 'Red Bull',
+    orderValue: 'Rp.500.000,00',
+    quantity: '10 Packets',
+    orderId: 1234,
+    expectedDelivery : '11/12/13',
+    status: 'Delayed'
   },
 ];
 const App = () => <Table columns={columns} dataSource={data} />;
